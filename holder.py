d8d1f14bd369dc0f8bfaa01f8c262a7925411f89
Goodjooy <42119608+Goodjooy@users.noreply.github.com>
2018-10-07 12:10:22 +0800

    Add files via upload

---
+++ holder.py
@@ -0,0 +1,156 @@
+import os
+
+import PIL.Image as Image
+
+
+class CutSpace(object):
+
+    def __init__(self):
+        self._xy = [0, 0]
+        self._size = [0, 0]
+        self._orig = [0, 0]
+        self._offset = [0, 0]
+        self._rotate = False
+
+    def cuter(self, pic):
+        """cut every part """
+        """[pygame.Surface,(x,y)]:returns"""
+        cut_sp = list(self._xy)
+        cut_sp.extend([self._size[0] + self._xy[0], self._size[1] + self._xy[1]])
+
+        if self._rotate:
+            cut_sp = list(self._xy)
+
+            cut_sp.extend([self._size[1] + self._xy[0], self._size[0] + self._xy[1]])
+
+            cut = pic.rotate(-90)
+            cut = pic.crop(cut_sp)
+
+        else:
+            cut = pic.crop(cut_sp)
+
+        x, y = self._xy
+
+        return [cut, (x, y)]
+
+    def add_xy(self, x, y):
+        self._xy = (x, y)
+
+    def add_size(self, size_x, size_y):
+        self._size = (size_x, size_y)
+
+    def add_rotate(self, rotate_90=False):
+        self._rotate = rotate_90
+
+    def add_orig(self, orig_x, orig_y):
+        self._orig = (orig_x, orig_y)
+
+    def add_offset(self, offset_x, offset_y):
+        self._offset = (offset_x, offset_y)
+
+    def all_to_0(self):
+        self._xy = ()
+        self._size = ()
+        self._orig = (0, 0)
+        self._offset = (0, 0)
+        self._rotate = False
+
+
+def body_cut(name):
+    try:
+        os.makedirs("out\\" + name)
+    except FileExistsError:
+        pass
+
+    pic = Image.open("Texture2D\\" + name + ".png")
+    space = {'body_part': {}}
+    loader = CutSpace()
+    in_part = None
+
+    with open("TextAsset\\" + name + ".atlas.txt", 'r')as info:
+        for line in info.readlines():
+
+            line = line[:-1]
+
+            if not line:
+                continue
+
+            elif line[-3:] == "png":
+                space['name'] = line[:-4]
+
+            elif line[0:4] == "size":
+                line = line[5:]
+                line = line.split(',')
+                line = [int(line[0]), int(line[1])]
+                space['size'] = line
+
+            elif line[:6] == "format":
+                space["format"] = line[8:-4]
+
+            elif line[:6] == "filter":
+                line = line[8:]
+                line = line.split(',')
+                space["filter"] = line
+
+            elif line[:6] == "repeat":
+                if line[8:] == 'none':
+                    space['repeat'] = None
+                else:
+                    space['repeat'] = line[8:]
+
+            else:
+                if line[0] != ' ':
+                    in_part = line
+                    space['body_part'][line] = None
+                    loader.all_to_0()
+
+                elif line[0] == ' ':
+                    line = line[2:]
+
+                    if line[:6] == 'rotate':
+                        line = line[8:]
+                        if line == "false":
+                            loader.add_rotate(False)
+                        elif line == "true":
+                            loader.add_rotate(True)
+
+                    elif line[:2] == 'xy':
+                        line = line[4:]
+                        line = line.split(',')
+                        line = [int(line[0]), int(line[1])]
+                        loader.add_xy(line[0], line[1])
+
+                    elif line[:4] == 'size':
+                        line = line[6:]
+                        line = line.split(',')
+                        line = [int(line[0]), int(line[1])]
+                        loader.add_size(line[0], line[1])
+
+                    elif line[:4] == 'orig':
+                        line = line[6:]
+                        line = line.split(',')
+                        line = [int(line[0]), int(line[1])]
+                        loader.add_orig(line[0], line[1])
+
+                    elif line[:6] == "offset":
+                        line = line[8:]
+                        line = line.split(',')
+                        line = [int(line[0]), int(line[1])]
+                        loader.add_offset(line[0], line[1])
+
+                    elif line[:5] == "index":
+                        space['body_part'][in_part] = loader.cuter(pic)
+
+    i = 0
+    for key in space['body_part'].keys():
+
+        if space['body_part'][key][0] is None:
+            continue
+
+        else:
+
+            space['body_part'][key][0].save("out\\" + name + "\\" + key + ".png")
+
+        i += 1
+
+    return True
---
+++ main_part.py
@@ -0,0 +1,46 @@
+import os
+import time
+
+import holder as ch
+
+try:
+    os.makedirs("texture2D")
+except FileExistsError:
+    pass
+try:
+    os.makedirs("textAsset")
+except FileExistsError:
+    pass
+try:
+    os.makedirs("out")
+except FileExistsError:
+    pass
+
+texture2D = os.listdir('texture2D')
+textAsset = os.listdir('textAsset')
+text_ = []
+for text in textAsset:
+    text_.append(text.split('.'))
+textAsset = []
+for text in text_:
+    textAsset.append(text[0])
+textAsset = set(textAsset)
+text_ = []
+for text in texture2D:
+    text_.append(text.split('.'))
+
+texture2D = []
+for text in text_:
+    texture2D.append(text[0])
+
+for name in texture2D:
+    if name not in textAsset:
+        print("切分文件丢失，请添加【"+name+".atlas.txt】至TextAsset文件夹" )
+    else:
+        if ch.body_cut(name):
+
+            print(f'完成一个，为{name}')
+
+print("完成，将于15s后关闭")
+
+time.sleep(15)
